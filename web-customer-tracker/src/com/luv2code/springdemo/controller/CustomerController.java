3433b2e65c02e397802cbacf0b60b0744070baaf
Duy Pham <duypham@Duys-MacBook-Pro.local>
2020-09-03 19:59:36 +0700

    Build Database Web App: Adding Function AddCustomer

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.entity.Customer;
@@ -28,6 +30,24 @@ public class CustomerController {
 		model.addAttribute("customers", customers);
 
 		return "list-customers";
+	}
+
+	@GetMapping("showFormForAdd")
+	public String showFormForAdd(Model model) {
+		// create model attribute to bind the form data
+		Customer customer = new Customer();
+
+		model.addAttribute("customer", customer);
+
+		return "customer-form";
+	}
+
+	@PostMapping("saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+		// save the customer using service
+		customerService.saveCustomer(customer);
+
+		return "redirect:/customer/list";
 	}
 
 }
